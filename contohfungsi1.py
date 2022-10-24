76f1723751bf91217e6ccf1501b59796880f4cb8
fahrans <103335400+fahrans@users.noreply.github.com>
2022-10-25 00:21:11 +0700

    Add files via upload

---
+++ contohfungsi1.py
@@ -0,0 +1,3 @@
+Buku = ['Bumi Manusia asli', 'Anak Semua Bangsa asli', 'Jejak Langkah bajakan', 'كتاب البيوع bajakan', 'Arok Dedes asli']
+for aspal in Buku:
+  print(aspal.upper())
---
+++ contohfungsi2.py
@@ -0,0 +1,3 @@
+Buku = ['Bumi Manusia asli', 'Anak Semua Bangsa asli', 'Jejak Langkah bajakan', 'كتاب البيوع bajakan', 'Arok Dedes asli']
+if "bajakan" in Buku[3]:
+  print("jangan beli " + Buku[3])
